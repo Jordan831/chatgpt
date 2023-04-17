6e7129557aaea8b0d1877c81c70d7fdc8bdfd9a0
Jordan831 <vicky.electron831@gmail.com>
2023-04-17 23:17:46 +0530

    added

--- controllers/user.js
+++ controllers/user.js
@@ -65,11 +65,10 @@ const completion = await openai.createChatCompletion({
 model: "gpt-3.5-turbo",
 max_tokens: 3500,
 temperature: 0.2,
-stop:['\n'],
 messages: [
 {
 role: "user", 
-content: `Write html paragraph code Description for "${input.Title.trim()}" 200 words and add  5 Special Features in html list code and create  Specification  table of  ${tableheading}  and  ${tablevalue}\n`}]
+content: `Write html paragraph code Description for "${input.Title.trim()}" 200 words and add  5 Special Features in html list code and create  Specification  table of  ${tableheading}  and  ${tablevalue}`}]
 });
 
 const query = `{
